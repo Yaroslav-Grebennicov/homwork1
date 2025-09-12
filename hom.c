ed8618d78e6c8c5af391b8aaed6e937bda7db49e
Ярослав Гребенников <yaroslav.grebennicov@icloud.com>
2025-09-12 15:02:37 +0300

    Add files via upload

---
+++ hom.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main()
+{
+    printf("         /---\\\n");
+    printf("        /     \\\n");
+    printf("       /       \\\n");
+    printf("      /_________\\\n");
+    printf("     |           |\n");
+    printf("     |  []   []  |\n");
+    printf("     |  --   --  |\n");
+    printf("     |  []   []  |\n");
+    printf("     |  --   --  |\n");
+    printf("     |    ___    |\n");
+    printf("     |____|_|____|\n");
+
+    return 0;
+}
